5fccff84c123bed954f836aa3bda24e67f318599
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-06-09 23:39:55 +0600

    created getLoggedInRecruiterCompany to add company information in post form

--- src/lib/api/companies.jsx
+++ src/lib/api/companies.jsx
@@ -1,6 +1,14 @@
 
 import { serverFetch } from "../cors/server"
+import { getUserSession } from "../cors/session"
 
 export const getRecruiterCompany = async(recruiterId) =>{
   return serverFetch(`/api/my/companies?recruiterId=${recruiterId}`)
+}
+
+// loggedin User
+
+export const  getLoggedInRecruiterCompany  = async () =>{
+  const user = await getUserSession();
+  return getRecruiterCompany(user?.id)
 }
--- src/lib/cors/session.js
+++ src/lib/cors/session.js
@@ -3,7 +3,7 @@ import { auth } from "../auth"
 
 export const getUserSession = async () =>{
       const session =  await auth.api.getSession({
-    headers: await headers() // some endpoints might require headers
+      headers: await headers() // some endpoints might require headers
 })
 
 return session?.user || null ;
